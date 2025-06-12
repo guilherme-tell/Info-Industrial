837e4cfe0cb7d60c711b1db58c525b7f2fdea704
guilherme-tell <guilhermesantos2700@gmail.com>
2025-06-12 16:56:18 -0300

    Update main.py

--- Servidor/main.py
+++ Servidor/main.py
@@ -1,5 +1,5 @@
 from servidor import Servidor
 
-serv = Servidor("localhost",9000)
+serv = Servidor("localhost",9000) #127.0.0.1 qualquer interface de rede do computador vai receber o serviço
 serv.start()
 
